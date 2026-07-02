d06ad59541f51717dbab6152996615fce982fe0e
Miyamoto Kaei <kaei199919@gmail.com>
2026-07-02 12:20:10 +0900

    preludeってpub useするだけでサブモジュールは持たないのね

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+/// AlpacaHackのURLの構造体のあるモジュール
+mod alpacahack_url;
+/// 問題の情報を持つための構造体
+mod challenge_info;
 /// 問題ページから問題の情報を取得する機能のモジュール
 mod fetch;
 /// ユビキタス言語っぽいやつ
--- src/prelude.rs
+++ src/prelude.rs
@@ -1,7 +1,2 @@
-/// AlpacaHackのURLの構造体のあるモジュール
-pub(crate) mod alpacahack_url;
-/// 問題の情報を持つための構造体
-pub(crate) mod challenge_info;
-
-pub(crate) use alpacahack_url::AlpacaHackUrl;
-pub(crate) use challenge_info::{ChallengeFile, ChallengeInfo, ChallengeMeta};
+pub(crate) use crate::alpacahack_url::AlpacaHackUrl;
+pub(crate) use crate::challenge_info::{ChallengeFile, ChallengeInfo, ChallengeMeta};
